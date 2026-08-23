fde7124cb0f3e23fd103ade82c67496b77c71b78
pretojoia3083 <luisrenatotrader@gmail.com>
2026-08-23 10:33:25 -0300

    chore: force redeploy

--- fix_repo.bat
+++ fix_repo.bat
@@ -1,7 +1,8 @@
 @echo off
 cd /d "C:\Users\Renato\Desktop\projetos code ia\LEVERAGE ARB"
+echo. >> .gitignore
 git add .
-git commit -m "feat: redesign - cards precos com verde quando oportunidade, painel compacto, mobile"
+git commit -m "chore: force redeploy"
 git push origin master:main --force
 echo DONE
 pause
